70c56924a7730735a5a58acad5fb80e1936d39c1
medson01 <medson01@gmail.com>
2018-10-16 18:01:45 -0300

    internacao_lista.php visualizar lista de acordo com o mes. Implementação com select e script

--- files/internacao_lista.php
+++ files/internacao_lista.php
@@ -1,11 +1,25 @@
 <?php 
   
+  // retira os erros 
+ // error_reporting(0);
+
+
+ if(isset($_GET['mes'])){
+
+  $mes = $_GET['mes'];
+ 
+
+  }else{
+
+  $mes = date("m");
+  
+  }
  
   If( $_SESSION["perfil"] == "usuario"){
    $query = mysqli_query($conn,"SELECT internamento.id as autorizacao, internamento.nome as paciente, internamento.matricula as matricula, internamento.solicitante as solicitante, internamento.crm as crm, internamento.dat_entrada as dat_entrada, internamento.dat_saida as dat_saida , cid.cid ,usuarios.nome as credenciado, cid.dias as dias, internamento.motivo as motivo FROM `internamento` INNER JOIN usuarios on usuarios.id = internamento.id_usuario INNER JOIN cid on cid.id = internamento.id_cid WHERE usuarios.login = '".$login."' and Year(internamento.dat_entrada) = '".date("Y")."' order by internamento.id") or die("erro ao carregar consulta");
   }else{
 
-     $query = mysqli_query($conn,"SELECT internamento.id as autorizacao, internamento.nome as paciente, internamento.matricula as matricula, internamento.solicitante as solicitante, internamento.crm as crm, internamento.dat_entrada as dat_entrada, internamento.dat_saida as dat_saida , cid.cid ,usuarios.nome as credenciado, cid.dias as dias, internamento.motivo as motivo FROM `internamento` INNER JOIN usuarios on usuarios.id = internamento.id_usuario INNER JOIN cid on cid.id = internamento.id_cid WHERE  Year(internamento.dat_entrada) = ".date("Y")." order by internamento.id") or die("erro ao carregar consulta");
+     $query = mysqli_query($conn,"SELECT internamento.id as autorizacao, internamento.nome as paciente, internamento.matricula as matricula, internamento.solicitante as solicitante, internamento.crm as crm, internamento.dat_entrada as dat_entrada, internamento.dat_saida as dat_saida , cid.cid ,usuarios.nome as credenciado, cid.dias as dias, internamento.motivo as motivo FROM `internamento` INNER JOIN usuarios on usuarios.id = internamento.id_usuario INNER JOIN cid on cid.id = internamento.id_cid WHERE MONTH(internamento.dat_entrada) = ".$mes." and Year(internamento.dat_entrada) = ".date("Y")." order by internamento.id") or die("erro ao carregar consulta");
 
   }
 
@@ -13,6 +27,12 @@
 
 
 ?>
+<!-- pegar mes de consulta -->
+<script language="Javascript">
+ document.getElementById("mes").onchange = function(){
+      window.location.href = this.value;
+    }
+</script>
 
 <!-- Mensagem ao passar o mouse -->
 <script type="text/javascript" src="../js/wz_tooltip.js"></script>
@@ -61,13 +81,27 @@ function excluir(id) {
      }
 }
 </script>
-
-
-
+<div align="right"><span style="right:inherit">Mês
+  <select name="m" id="m" onchange="javascript:location.href = this.value;">
+    <option  value="" > ... </option>
+    <option  value="internacao.php?mes=01"<?php  if($mes == '01'){ echo "selected"; } ?> >Janeiro </option>
+    <option  value="internacao.php?mes=02"<?php  if($mes == '02'){ echo "selected"; } ?>>Fevereiro</option>
+    <option  value="internacao.php?mes=03"<?php  if($mes == '03'){ echo "selected"; } ?>>Março</option>
+    <option  value="internacao.php?mes=04"<?php  if($mes == '04'){ echo "selected"; } ?>>abril</option>
+    <option  value="internacao.php?mes=05"<?php  if($mes == '05'){ echo "selected"; } ?>>Maio</option>
+    <option  value="internacao.php?mes=06"<?php  if($mes == '06'){ echo "selected"; } ?>>Junho</option>
+    <option  value="internacao.php?mes=07"<?php  if($mes == '07'){ echo "selected"; } ?>>Julho</option>
+    <option  value="internacao.php?mes=08"<?php  if($mes == '08'){ echo "selected"; } ?>>Agosto</option>
+    <option  value="internacao.php?mes=09"<?php  if($mes == '09'){ echo "selected"; } ?>>Setembro</option>
+    <option  value="internacao.php?mes=10"<?php  if($mes == '10'){ echo "selected"; } ?>>Outubro</option>
+    <option  value="internacao.php?mes=11"<?php  if($mes == '11'){ echo "selected"; } ?>>Novembro</option>
+    <option  value="internacao.php?mes=12"<?php  if($mes == '12'){ echo "selected"; } ?>>dezembro</option>
+  </select>
+</span></div>
                     
    <table class="table table-striped" align="center" style="font-size: 9px">
                <tr>
-                 <td colspan="12" style="text-align: center; text-decoration-style: solid;"> <strong>Pacientes insternados</strong></td>
+                 <td colspan="12" style="text-align: center; text-decoration-style: solid;"> <strong>Pacientes insternados </strong></td>
                </tr>
                <tr  style='font-weight:bold;'>
                  <!-- <td width="27"><div align="center">Status</div></td> -->
@@ -188,7 +222,7 @@ function excluir(id) {
                    
               ?>              
             
-        </table>
+</table>
 			       <span style="background-color: red"></span>
 					
 	
