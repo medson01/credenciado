57547c784d126418855b09a1da9cc0157285dbbf
medson01 <32308424+medson01@users.noreply.github.com>
2024-09-03 15:51:45 -0300

    Terminon do projeto Consulta de Situação: adicionada funcionalidade de consultar dados do Titular.

--- files/fom_consulta_situacao.php
+++ files/fom_consulta_situacao.php
@@ -83,31 +83,39 @@ function iFrameHeight()
 	 <iframe src="iframe_form_consulta_situacao.php" height="900" width="100%" scrolling="no" style="border:none;"></iframe>
 -->
 
- <div class="panel panel-info ">
-      <div class="panel-heading">
-        <div align="center">
-          <p><strong>ATEN&Ccedil;&Atilde;O</strong></p>
-          <p align="justify">
-            <span class="style2">&nbsp;&nbsp;&nbsp;A CONSULTA DE SITUA&Ccedil;&Atilde;O apresenta as situa&ccedil;&otilde;es "Ativo" ou "Bloqueado" de cada componente da fam&iacute;lia. Al&eacute;m disso, possibilita o pagamento dos meses em aberto atrav&eacute;s do link &quot;GERAR BOLETO&quot;. Efetuando o pagamento, ap&oacute;s 15 minutos, o sistema dar baixa. Caso o usu&aacute;rio queira, ele ainda pode entra em contato com o financeiro do Ipaseal pelo WhatsApp (<strong>82</strong>) <strong>98812-9043</strong>.</span></p>
-        </div>
-      </div>
-
-		<form name="consulta_situacao" id="consulta_situacao" action ="fom_consulta_situacao.php"  method="post" data-parsley-validate class="form-horizontal form-label-left">
+ <div class="panel panel-info">
+   	<div class="panel-heading">			Consulte o CPF do titular para a Consulta de Situa&ccedil;&atilde;o:	</div>
+        <div class="panel-body">
+	  <form name="consulta_situacao" id="consulta_situacao" action ="fom_consulta_situacao.php"  method="post" data-parsley-validate class="form-horizontal form-label-left">
         <br />
-			&nbsp;&nbsp;&nbsp; Descubra o CPF do titular para a consulta abaixo, digite a matr&iacute;cula de qualquer componente familiar:
-		<input required="required" type="text" name="matricula" minlength="16" class="form-matric" id="matricula" size="20" maxlength="18" placeholder="00000000.000000.00" onchange="pegarMatricula()" />
+			&nbsp;&nbsp;&nbsp; Digite a matr&iacute;cula de qualquer componente familiar:
+		    <input required="required" type="text" name="matricula" minlength="16" class="form-matric" id="matricula" size="20" maxlength="18" placeholder="00000000.000000.00" onchange="pegarMatricula()" />
 			  <input name="submit" type="Submit" value="Pesquisar" class="btn btn-primary " />
 		</form>
 <hr />
 		<?php 
 			if(isset($frame_nome)){
 			 		echo "&nbsp;&nbsp;&nbsp; Nome do titular : ".$frame_nome."<br>";
-					echo "&nbsp;&nbsp;&nbsp; Matr&iacute;cula da fam&iacute;lia : ".$frame_matricula."<br>";
-					echo "&nbsp;&nbsp;&nbsp; CPF : ".$frame_cpf;
-					echo "<hr />";
+					echo "&nbsp;&nbsp;&nbsp; CPF : ".$frame_cpf."<br>";
+					echo "&nbsp;&nbsp;&nbsp; Matr&iacute;cula: ".$frame_matricula;
+					
+					
 			}
 		
 		?>
+	  </div>
+    </div>
+
+ <div class="panel panel-info ">
+      <div class="panel-heading">
+        <div align="center">
+          <p><strong>ATEN&Ccedil;&Atilde;O</strong></p>
+          <p align="justify">
+            <span >&nbsp;&nbsp;&nbsp;A CONSULTA DE SITUA&Ccedil;&Atilde;O apresenta as situa&ccedil;&otilde;es "Ativo" ou "Bloqueado" de cada componente fam&iacute;liar. Al&eacute;m disso, possibilita o pagamento dos meses em aberto atrav&eacute;s do link &quot;GERAR BOLETO&quot;,  que 15 minutos ap&oacute;s, o sistema dar&aacute; baixa automaticamente. Caso o usu&aacute;rio queira, ele ainda pode entra em contato com o financeiro do Ipaseal pelo WhatsApp (<strong>82</strong>) <strong>98812-9043</strong>.</span></p>
+        </div>
+      </div>
+
+		
 
 	<div id="IframePos">
 <iframe id="blockrandom" name="iframe" src="http://sistemasweb.itec.al.gov.br/ipaseal/situacao_contrato/" width="100%" height="800"  scrolling="no"  style="border:none;" class="wrapper">
